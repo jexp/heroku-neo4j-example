0c888dd3a0ffc64dbf76467a01a1ff42a228e198
maxdemarzi <maxdemarzi@hotmail.com>
2011-02-10 11:18:51 -0800

    with v2 authentication working

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source :gemcutter
 gem 'sinatra', '1.1.2'
-gem 'neography', '0.0.9'
+gem 'neography', '0.0.10'
 gem 'haml', '3.0.25'
 
 group :test do
--- imdb.rb
+++ imdb.rb
@@ -22,7 +22,9 @@ configure do
     end
 
   Config.server = ENV['NEO4J_HOST']
-  Config.authentication = {:basic_auth => {:username => ENV['NEO4J_LOGIN'], :password => ENV['NEO4J_PASSWORD']}}
+  Config.authentication = 'basic'
+  Config.username = ENV['NEO4J_LOGIN']
+  Config.password = ENV['NEO4J_PASSWORD']
 
 end
 
@@ -46,8 +48,7 @@ helpers do
 end
 
 get '/' do
-   '<h2>Neo4j Imdb</h2>' + @neo.get_root.inspect + '<br\>' + '<h3>Indexes</h3>' + @neo.list_indexes.inspect 
-     
+   '<h2>Neo4j Imdb</h2>' + @neo.get_root.inspect + '<br\>' + '<h3>Indexes</h3>' + @neo.list_node_indexes.inspect 
 end
 
 get '/movie/:id' do
@@ -74,18 +75,22 @@ get '/actor/:id' do
   haml :show_actor
 end
 
+get '/v2' do
+   '<h2>Neo4j Imdb</h2>' + Neography.ref_node.inspect + '<br\>' + '<h3>Node Indexes</h3>' + @neo.list_node_indexes.inspect + '<h3>Node Indexes</h3>' + @neo.list_relationship_indexes.inspect 
+end
+
 get '/actor_v2/:id' do
   @actor = Node.load(params[:id])  
-  @movies = @actor.outgoing("ACTS_IN")
+  @roles = @actor.rels("ACTS_IN").outgoing
 
   haml :show_actor_v2
 end
 
 get '/movie_v2/:id' do
   @movie = Node.load(params[:id])  
-  @actors = @actor.outgoing("ACTS_IN")
+  @roles = @movie.rels("ACTS_IN").incoming
 
-  haml :show_actor_v2
+  haml :show_movie_v2
 end
 
 
